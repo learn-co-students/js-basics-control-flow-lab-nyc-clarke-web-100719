c8da6f2b0df89aa8fd017f0bd26d1d2f79bee1a2
Ryan <rngao97@gmail.com>
2019-11-16 16:22:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if(number <= 400){
+    return 'This one is on me!';
+  }
+  else if (number <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (number > 2500){
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location){
+   if(location === 'NYC'){
+    return 'Ok, sounds good.';
+   }
+   else return 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+      break;
+  }
 }
