884a550a6347139c9afd9739e1721c6709e0faff
soufianeou <soufiane.oucherrou@gmail.com>
2019-11-17 10:56:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(arg){
   // Write your code here!
+  let answer = '';
+if (arg <= 400){
+answer = 'This one is on me!';
+} else if (arg > 2000 && arg < 2500){
+answer = "I will gladly take your thirty bucks.";
+} else {
+  answer = "No can do."
+}
+return answer;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let answer ='';
+  city === "NYC" ? (answer = "Ok, sounds good.") : (answer = "No go.");
+  return answer;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(word){
   // Write your code here!
+  let answer = '';
+  if (word === "generous") {
+    answer = 'Thank you so much.';
+  } else if (word === "not as generous") {
+    answer = "Thank you.";
+  } else {
+    answer = "Bye.";
+  }
+  return answer;
 }
