564384e344375ff164cc659f96489da789399acb
Barbara <muesingb@gmail.com>
2019-11-18 16:11:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride > 2000 && ride < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    return 'No can do.'
+  };
+};
+
+function ternaryCheckCity(city){
+	return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){ //Use a switch statement to return a different 
+	//response based on the generosity of the passenger's tip.
+  switch(tip) {
+		case 'generous':
+			return 'Thank you so much.';
+			break;
+		case 'not as generous':
+			return 'Thank you.';
+			break;
+		default:
+			return 'Bye.';
+			break;
+	}
 }
